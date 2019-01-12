6b2a9fb2ff550516628c3c7d3b3d2febdfcaa535
CoolNamesAllTaken <jmcnelly@stanford.edu>
2019-01-12 14:57:51 -0800

    Make servos go back and forth

--- src/servo_gang_controller/servo_gang_controller.ino
+++ src/servo_gang_controller/servo_gang_controller.ino
@@ -2,28 +2,71 @@
 #define SERCLK_PIN 3
 #define RCLK_PIN 4
 
-#define SR_NUM_BITS 8
+#define SR_NUM_BITS 8 // also number of servos
+
+#define SERVO_PERIOD_US 20000 // 50 Hz
+#define SERVO_SHORT_US 500
+#define SERVO_LONG_US 2500
 
 uint8_t sr_buffer = 0;
+uint8_t servo_positions = 0; // 0 = closed, 1 = open
+
+unsigned long start_us = 0;
+unsigned long last_us = 0;
+
+unsigned long last_servo_change_ms = 0;
 
 void setup() {
 	Serial.begin(115200);
+
+	// set pinmodes
 	pinMode(SER_PIN, OUTPUT);
 	pinMode(SERCLK_PIN, OUTPUT);
 	pinMode(RCLK_PIN, OUTPUT);
 
+	// inititalize digital pins LOW
 	digitalWrite(SER_PIN, LOW);
 	digitalWrite(SERCLK_PIN, LOW);
 	digitalWrite(RCLK_PIN, LOW);
+
+	start_us = micros();
+	servo_positions = 0b10101010;
 }
 
 void loop() {
-	sr_buffer = 0b10101010;
-	sr_write();
-	delay(5000);
-	sr_buffer = 0b01010101;
-	sr_write();
-	delay(5000);
+	if (millis() - last_servo_change_ms > 500) {
+		servo_positions = ~servo_positions;
+		last_servo_change_ms = millis();
+	}
+	update_sr_buffer();
+}
+
+void update_sr_buffer() {
+	unsigned long curr_us = micros() - start_us;
+	if (curr_us >= SERVO_PERIOD_US) {
+		// end of counting period
+		start_us = micros(); // reset time counter
+		sr_buffer = 0xff; // servo signals all start HIGH
+	} else if (curr_us > SERVO_SHORT_US && last_us < SERVO_SHORT_US) {
+		// set position signal for servos that are open
+		for (int i = 0; i < SR_NUM_BITS; i++) {
+			if (servo_positions & (1 << i)) {
+				// servo is open
+				sr_buffer &= ~(1 << i);
+			}
+		}	
+	} else if (curr_us > SERVO_LONG_US && last_us < SERVO_LONG_US) {
+		// set position signal for servos that are closed
+		for (int i = 0; i < SR_NUM_BITS; i++) {
+			if (!(servo_positions & (1 << i))) {
+				// servo is closed
+				sr_buffer &= ~(1 << i);
+			}
+		}
+	}
+
+	last_us = curr_us;
+	sr_write(); // update servo pins
 }
 
 void sr_write() {
